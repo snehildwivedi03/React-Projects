be458833148ec5476f952c9e4077a9bff8dc02aa
snehildwivedi03 <snehildwivedi03@gmail.com>
2024-12-28 20:55:26 +0530

    Css

--- Concept-2.1-Bootstrap/Concept-2.2-bs/src/App.jsx
+++ Concept-2.1-Bootstrap/Concept-2.2-bs/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    // buttons div concepts
+    // buttons div concept
     <div>
       <button type="button" class="btn btn-primary">
         Primary
