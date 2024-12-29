6c005eab7617968e54291fba4b62ab0b93202646
snehildwivedi03 <snehildwivedi03@gmail.com>
2024-12-29 14:47:08 +0530

    Todo app structure

--- Projects/1-To-Do-App-V-1/src/App.jsx
+++ Projects/1-To-Do-App-V-1/src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <center className="todo-container">
+      <h1>TODO App</h1>
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-6">
+            <input type="text" placeholder="Enter todo here" />
+          </div>
+          <div class="col-4">
+            <input type="date" />
+          </div>
+          <div class="col-2">
+            <button type="button" class="btn btn-success">
+              Add
+            </button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      {/* Row 2 */}
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-6">Buy Milk </div>
+          <div class="col-4">21/12/2024 </div>
+          <div class="col-2">
+            <button type="button" class="btn btn-danger">
+              Remove
+            </button>
+          </div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+      {/* Row 3 */}
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-6">Buy Protein </div>
+          <div class="col-4">02/01/2025 </div>
+          <div class="col-2">
+            <button type="button" class="btn btn-danger">
+              Remove
+            </button>
+          </div>
+        </div>
+      </div>
+    </center>
+  );
 }
 
-export default App
+export default App;
