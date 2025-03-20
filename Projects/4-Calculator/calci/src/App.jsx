52db3ebffee4ed9274b716eea074dfcce6442576
snehildwivedi03 <snehildwivedi03@gmail.com>
2025-03-20 17:56:59 +0530

    Basic Structure

---
+++ Projects/4-Calculator/calci/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div id="calculator">
+      <input type="text" id="display" />
+      <div id="btn-container"></div>
+      <button>C</button>
+    </div>
+  );
+}
+
+export default App;
