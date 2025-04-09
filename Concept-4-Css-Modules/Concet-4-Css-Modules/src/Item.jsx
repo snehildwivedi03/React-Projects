fe032c96cd574540be18db2de6382abbd0484aa8
snehildwivedi03 <snehildwivedi03@gmail.com>
2025-04-09 17:00:08 +0530

    Changes

--- Concept-4-Css-Modules/Concet-4-Css-Modules/src/Item.jsx
+++ Concept-4-Css-Modules/Concet-4-Css-Modules/src/Item.jsx
@@ -3,7 +3,7 @@ const Item = ({ foodItem, handleBuyBtn }) => {
   return (
     <li className={`${styles["my-item"]} list-group-item`}>
       <span className={styles["my-span"]}>{foodItem}</span>
-      {/* {Simple Btn} */}
+
       <button
         className={`${styles.buyBtn} btn btn-info`}
         onClick={handleBuyBtn}
