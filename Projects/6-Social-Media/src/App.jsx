7f016c79f6cdef87f9a93a223efd44479a3f6dbf
snehildwivedi03 <snehildwivedi03@gmail.com>
2025-04-19 15:19:26 +0530

    Basic-Layout

--- Projects/6-Social-Media/src/App.jsx
+++ Projects/6-Social-Media/src/App.jsx
@@ -1,10 +1,30 @@
 import "./App.css";
-
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
+import CreatePostForm from "./components/CreatePostForm";
+import PostList from "./components/PostList";
+import { useState } from "react";
 function App() {
+  const [selectedTab, setSelectedTab] = useState("Home");
+
   return (
-    <>
-      <h1>Social Media</h1>
-    </>
+    <div className="app-container">
+      <Sidebar
+        selectedTab={selectedTab}
+        setSelectedTab={setSelectedTab}
+      ></Sidebar>
+      <div className="content">
+        <Header></Header>
+
+        {selectedTab === "Home" ? (
+          <PostList></PostList>
+        ) : (
+          <CreatePostForm></CreatePostForm>
+        )}
+        <Footer></Footer>
+      </div>
+    </div>
   );
 }
 
