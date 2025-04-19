9597ebda0c76b87ff208f926bc24487bda526a43
snehildwivedi03 <snehildwivedi03@gmail.com>
2025-04-19 12:45:19 +0530

    Final Version

--- Projects/3-To-Do-App-V-2 /src/App.jsx
+++ Projects/3-To-Do-App-V-2 /src/App.jsx
@@ -2,64 +2,14 @@ import AppName from "./components/AppName";
 import AddTodo from "./components/AddTodo";
 import WelcomeMsg from "./components/WelcomeMsg";
 import ToDoItems from "./components/ToDoItems";
-import { TodoItemContext } from "./store/ToDoItems-Store";
+import ToDoItemsContextProvider from "./store/ToDoItems-Store";
 import "./App.css";
-import { useReducer, useState } from "react";
-
-const toDoItemReducer = (currToDoItem, action) => {
-  let newToDoItems = currToDoItem;
-  if (action.type === "NEW_ITEM") {
-    newToDoItems = [
-      ...currToDoItem,
-      { name: action.payload.itemName, date: action.payload.itemDueDate },
-    ];
-  } else if (action.type === "DELETE_ITEM") {
-    newToDoItems = currToDoItem.filter(
-      (item) => item.name != action.payload.itemName
-    );
-  }
-
-  return newToDoItems;
-};
 
 function App() {
   // const [toDoItems, setToDoItems] = useState([]);
-  const [toDoItems, dispatchToDoItems] = useReducer(toDoItemReducer, []);
 
-  const addNewItem = (itemName, itemDueDate) => {
-    const newItemAction = {
-      type: "NEW_ITEM",
-      payload: {
-        itemName,
-        itemDueDate,
-      },
-    };
-    dispatchToDoItems(newItemAction);
-
-    /* setToDoItems((currValue) => [
-      ...currValue,
-      { name: itemName, date: itemDueDate },
-    ]);*/
-  };
-  const deleteCurrItem = (toDoItemName) => {
-    const deleteItemAction = {
-      type: "DELETE_ITEM",
-      payload: {
-        itemName: toDoItemName,
-      },
-    };
-    dispatchToDoItems(deleteItemAction);
-    /* const newtoDoItem = toDoItems.filter((item) => item.name != toDoItemName);
-    setToDoItems(newtoDoItem);*/
-  };
   return (
-    <TodoItemContext.Provider
-      value={{
-        toDoItems,
-        addNewItem,
-        deleteCurrItem,
-      }}
-    >
+    <ToDoItemsContextProvider>
       <center className="todo-container">
         <AppName></AppName>
 
@@ -67,7 +17,7 @@ function App() {
         <WelcomeMsg></WelcomeMsg>
         <ToDoItems></ToDoItems>
       </center>
-    </TodoItemContext.Provider>
+    </ToDoItemsContextProvider>
   );
 }
 
--- Projects/3-To-Do-App-V-2 /src/store/ToDoItems-Store.jsx
+++ Projects/3-To-Do-App-V-2 /src/store/ToDoItems-Store.jsx
@@ -1,6 +1,59 @@
 import { createContext } from "react";
+import { useReducer } from "react";
+
 export const TodoItemContext = createContext({
   toDoItems: [],
   addNewItem: () => {},
   deleteCurrItem: () => {},
 });
+const toDoItemReducer = (currToDoItem, action) => {
+  let newToDoItems = currToDoItem;
+  if (action.type === "NEW_ITEM") {
+    newToDoItems = [
+      ...currToDoItem,
+      { name: action.payload.itemName, date: action.payload.itemDueDate },
+    ];
+  } else if (action.type === "DELETE_ITEM") {
+    newToDoItems = currToDoItem.filter(
+      (item) => item.name != action.payload.itemName
+    );
+  }
+
+  return newToDoItems;
+};
+const ToDoItemsContextProvider = ({ children }) => {
+  const [toDoItems, dispatchToDoItems] = useReducer(toDoItemReducer, []);
+
+  const addNewItem = (itemName, itemDueDate) => {
+    const newItemAction = {
+      type: "NEW_ITEM",
+      payload: {
+        itemName,
+        itemDueDate,
+      },
+    };
+    dispatchToDoItems(newItemAction);
+  };
+
+  const deleteCurrItem = (toDoItemName) => {
+    const deleteItemAction = {
+      type: "DELETE_ITEM",
+      payload: {
+        itemName: toDoItemName,
+      },
+    };
+    dispatchToDoItems(deleteItemAction);
+  };
+  return (
+    <TodoItemContext.Provider
+      value={{
+        toDoItems,
+        addNewItem,
+        deleteCurrItem,
+      }}
+    >
+      {children}
+    </TodoItemContext.Provider>
+  );
+};
+export default ToDoItemsContextProvider;
