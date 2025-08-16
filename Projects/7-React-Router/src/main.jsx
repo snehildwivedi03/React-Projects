44d60752aa30ea7c5703932e124c625097ecc45b
snehildwivedi03 <snehildwivedi03@gmail.com>
2025-08-16 20:32:01 +0530

    Updated project

--- Projects/7-React-Router/src/main.jsx
+++ Projects/7-React-Router/src/main.jsx
@@ -1,10 +1,25 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import App from "./routes/App.jsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import CreatePostForm from "./components/CreatePostForm.jsx";
+import { PostList } from "./store/PostListStore.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <PostList /> },
+      { path: "/create-post", element: <CreatePostForm /> },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
+    {/* <App /> */}
   </StrictMode>
 );
--- Projects/7-React-Router/src/routes/App.jsx
+++ Projects/7-React-Router/src/routes/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Sidebar from "./components/Sidebar";
-import CreatePostForm from "./components/CreatePostForm";
-import PostList from "./components/PostList";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Sidebar from "../components/Sidebar";
+import CreatePostForm from "../components/CreatePostForm";
+import PostList from "../components/PostList";
 import { useState } from "react";
-import PostListProvider from "./store/PostListStore";
+import PostListProvider from "../store/PostListStore";
+import { Outlet } from "react-router-dom";
 function App() {
   const [selectedTab, setSelectedTab] = useState("Home");
 
@@ -18,12 +19,12 @@ function App() {
         ></Sidebar>
         <div className="content">
           <Header></Header>
-
-          {selectedTab === "Home" ? (
+          <Outlet />
+          {/* {selectedTab === "Home" ? (
             <PostList></PostList>
           ) : (
             <CreatePostForm></CreatePostForm>
-          )}
+          )} */}
           <Footer></Footer>
         </div>
       </div>
